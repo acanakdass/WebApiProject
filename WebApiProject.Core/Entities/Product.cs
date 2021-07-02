331c8841f0919aa72bb1dde414d6f0840ecf77ae
Ahmet Can Akdaş <acanakdas@gmail.com>
2021-07-03 00:43:11 +0300

    Add project files.

---
+++ WebApiProject.Core/Entities/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebApiProject.Core.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Stock { get; set; }
+        public int Price { get; set; }
+        public int CategoryId { get; set; }
+        public bool IsDeleted { get; set; }
+        public string InnerBarcode { get; set; }
+        public virtual Category Category { get; set; }
+    }
+}
---
+++ WebApiProject.Core/Repositories/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApiProject.Core.Repositories
+{
+    public interface IRepository<TEntity> where TEntity:class
+    {
+        Task<TEntity> GetByIdAsync(int id);
+        Task<IEnumerable<TEntity>> GetAllAsync();
+        Task<IEnumerable<TEntity>> Find(Expression<Func<TEntity, bool>> predicate);
+        Task<TEntity> Get(Expression<Func<TEntity, bool>> predicate);
+    }
+}
